1883f9f8ec679333a7862ff74be697611c45d5e4
AbdulghaniHarran <abdulghani@rafraf.com>
2022-08-23 00:14:12 +0300

    Revert "Navbar-Done"
    This reverts commit 16a174eeb63f3d7b3526e556b2f1be43884e9781.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,13 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Navbar from "../components/Navbar";
-import { useState, Fragment } from "react";
-import { Menu } from "@headlessui/react";
-import { useRouter } from "next/router";
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
-  const router = useRouter()
-  const [open, setOpen] = useState(false);
-
   return (
-      <div className="m-8">
-        Home Page
-      </div>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-/**
- * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
- */
- module.exports = {
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
   content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "./pages/**/*.{ts,tsx}",
-    "./public/**/*.html",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  plugins: [
-    require("flowbite/plugin")
-  ],
-  theme: {},
-};
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
