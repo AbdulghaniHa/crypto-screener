16a174eeb63f3d7b3526e556b2f1be43884e9781
AbdulghaniHarran <abdulghani@rafraf.com>
2022-08-23 00:11:51 +0300

    Navbar-Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import { useState, Fragment } from "react";
+import { Menu } from "@headlessui/react";
+import { useRouter } from "next/router";
 
 
 export default function Home() {
+  const router = useRouter()
+  const [open, setOpen] = useState(false);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+      <div className="m-8">
+        Home Page
+      </div>
+  );
 }
---
+++ pages/pumpDump.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+
+export default function pumpDump() {
+    return (
+        <div className="m-8">
+            <h4>
+                Pump and Dump page
+            </h4>
+        </div>
+    );
+  }
+  
